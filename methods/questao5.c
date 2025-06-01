6a684f12ac424ba37d7afa6e29b677a811f3c9c7
Vinny00101 <vini123gtbr@gmail.com>
2025-06-01 16:15:45 -0300

    feat: Função para calcular o total vendido por produto, o mais vendido e o valor total.

--- methods/questao5.c
+++ methods/questao5.c
@@ -2,13 +2,13 @@
 #include "limits.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 
 int extrair_Codigo_Arquivo(){
     FILE *arq = fopen("vendas.txt", "r");
     if(arq == NULL){
         printf("Erro a abrir o arquivo\n");
-        return 1;
+        return 0;
     }
 
     Produto p;
@@ -24,48 +24,149 @@ int extrair_Codigo_Arquivo(){
             max = p.codigo;
         }
     }
-    return max;
+    fclose(arq);
+    return max + 1;
 }
-// implementation of function
+
+void addVendidoTotal(float *maisVendido, int *index, float *total)
+{
+    FILE *arq = fopen("vendas.txt", "r");
+    FILE *temp = fopen("temp.txt", "w");
+    char linha[100];
+    int opcao;
+
+    if(!arq || !temp)
+    {
+        printf("Erro a abrir o arquivo\n");
+        return;
+    }
+
+    while (fgets(linha, sizeof(linha), arq))
+    {
+        if(strncmp(linha, "TOTAL;", 6) == 0)
+        {
+            continue;
+        }
+        if(strncmp(linha, "MAIS_VENDIDO;", 13) == 0)
+        {
+            continue;
+        }
+
+        fputs(linha, temp);
+    }
+    
+
+    fprintf(temp, "TOTAL;%2.f\n", *total);
+    fprintf(temp, "MAIS_VENDIDO;%d;%2.f\n", *index, *maisVendido);
+
+    fclose(arq);
+    fclose(temp);
+
+    remove("vendas.txt");
+    rename("temp.txt", "vendas.txt");
+
+    do
+    {
+        printf("Total geral: %f\n", *total);
+        printf("Produto mais vendido: %d (R$ %f)\n", *index, *maisVendido);
+        printf("Digite 1 para sair:");
+        scanf("%d",&opcao);
+
+    }while(opcao != 1);
+
+    *total = 0;
+    *index = 0, *maisVendido = 0;
+}
+
+int procesaDados(float *maisVendido, int *index, float *total)
+{
+    FILE *arq = fopen("vendas.txt", "r");
+    if(arq == NULL)
+    {
+        printf("Erro a abrir o arquivo\n");
+        return 0;
+    }
+    
+    Produto p;
+    char linha[100];
+    int opcao;
+    int validation = 1;
+    float max = 0;
+    do
+    {
+        printf("Processamento concluido:\n");
+        while (fgets(linha, sizeof(linha), arq))
+        {
+            if(!(sscanf(linha, "%d;%d;%f", &p.codigo, &p.quantidade, &p.preco_unitario) == 3))
+            {
+                continue;
+            }
+
+            max = p.quantidade * p.preco_unitario;
+            *total += max;
+
+            if(max > *maisVendido)
+            {
+                *maisVendido = max;
+                *index = p.codigo;
+            }
+            printf("Total produto %d: R$ %2.f\n",p.codigo, max);
+        }
+        
+        if(!(*total && *maisVendido && *index))
+        {
+            validation = 0;
+            printf("Nenhum produto adicionado\n");
+        }
+
+        printf("Digite 1 para sair:");
+        scanf("%d",&opcao);
+
+    }while(opcao != 1);
+    fclose(arq);
+
+    return validation;
+}
+
 void adicionaProduto(Produto *p, int *codigo){
+    printf("%d",*codigo);
     p->codigo = *codigo;
     printf("Informe a quantidade de produto\n");         
     scanf("%d", &p->quantidade);
     printf("Informe o preco unitario do produto\n");
     scanf("%f",&p->preco_unitario);
-    (*codigo)++;
+    (*codigo)++; 
 }
 
-int addprodutoFile(int *codigo){
+void addprodutoFile(int *codigo){
     FILE *arq = fopen("vendas.txt", "a");
     if(arq == NULL){
         printf("Erro a abrir o arquivo\n");
-        return 1;
+        return;
     }
-    (*codigo)++;
     Produto produto;
     adicionaProduto(&produto, codigo);
-
     fprintf(arq, "%d;%d;%.2f\n", produto.codigo, produto.quantidade, produto.preco_unitario);
     fclose(arq);
     printf("Produto adicionado com sucesso!\n");
-
-    return 0;
 }
 
-void mainQuestao5(){
-    int opcao, codigo;
+int mainQuestao5()
+{
+    int opcao, codigo, index=0;
+    float maisVendido=0, total=0;
     codigo = extrair_Codigo_Arquivo();
 
     do
     {
         system("cls");
-        printf("*-----------------Menu---------------*\n");
+        printf("-----------------Menu---------------\n");
         printf("|-----------Informe a opcao----------|\n");
-        printf("|____________________________________|\n");
-        printf("*1-Adiciona Produto---------------<<-*\n");
-        printf("*2-Sair---------------------------<<-*\n");
-        printf("*____________________________________*\n");
+        printf("||\n");
+        printf("1-Adiciona Produto---------------<<-\n");
+        printf("2-Processar Dados----------------<<-\n");
+        printf("3-Sair---------------------------<<-\n");
+        printf("\n");
         scanf("%d", &opcao);
 
         switch (opcao)
@@ -74,9 +175,16 @@ void mainQuestao5(){
             system("cls");
             addprodutoFile(&codigo);
             break;
-        
+        case 2:
+            system("cls");
+            if(procesaDados(&maisVendido,&index, &total))
+            {
+                addVendidoTotal(&maisVendido,&index,&total);
+            }
+            break;
         default:
             break;
         }
-    } while (opcao != 2);
+    } while (opcao != 3);
+    return 0;
 }
--- methods/questao5.h
+++ methods/questao5.h
@@ -1,6 +1,7 @@
 #ifndef QUESTAO_5
 #define QUESTAO_5
 #include <stdio.h>
+#include <string.h>
 
 // Declaration of function
 typedef struct
@@ -10,16 +11,16 @@ typedef struct
     float preco_unitario;
 }Produto;
 
-// Calcular total por produto function
-
-// Identificar produto mais vendido function
-
-// Calcular total geral function
-
-// Atualizar o arquivo com function
 int extrair_Codigo_Arquivo();
+// funcao para adiciona os produtos na struct
+void addVendidoTotal(float *maisVendido, int *index, float *total);
+// Atualizar o arquivo com function
+int procesaDados(float *maisVendido, int *index, float *total);
+// funcao para buscar o ultimo codigo de produto e depois retorna o codigo + 1 que sera o proximo depois do maior
 void adicionaProduto(Produto *p, int *codigo);
-int addprodutoFile(int *codigo);
-void mainQuestao5();
+// funcao que adiciona os dados no file
+void addprodutoFile(int *codigo);
+// funcao main
+int mainQuestao5();
 
 #endif
